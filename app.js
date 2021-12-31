2937abe62cf2c18047fbfa4f21f02ae10cfd5088
Chidinma Okwuoha <nancee801@gmail.com>
2021-12-31 12:45:13 +0100

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 Vue.createApp({
   data() {
     return {
-      goals: [],
-      enteredValue: ''
+      isEditing:false,
+      todos: ['go to the market','cook','sweep'],
+      todo: '',
+      selectedIndex:null
     };
   },
   methods: {
-    addGoal() {
-      this.goals.push(this.enteredValue);
-      this.enteredValue = '';
+    storeTodo() {
+       this.todos.push(this.todo);
+       this.todo=' '
+    },
+    editTodo(index,todo){
+       this.todo=todo
+       this.selectedIndex=index
+       this.isEditing=true
+    },
+    updateTodo(){
+      this.todos.splice(this.selectedIndex,1,this.todo)
+      this.isEditing = false
+    },
+    deleteTodo(index){
+      this.todos.splice(index,1)
     }
   }
 }).mount('#app');
